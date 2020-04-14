3f3aa6f3956b9f468ce8fb7961aca28270c079c2
Mark <pal.mark28@gmail.com>
2020-04-14 10:23:54 +0200

    remove RMR_GUI from project

--- RMR_GUI/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// RMR_GUI.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-#undef NOMINMAX
-#include "stdafx.h"
-
-
